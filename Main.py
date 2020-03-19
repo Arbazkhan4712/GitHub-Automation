3ea63baea70c7d5d8dde6db324901470e9c802cf
Arbaz Khan <49393716+Arbazkhan4712@users.noreply.github.com>
2020-03-19 13:08:29 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from time import sleep
+from pass import pw,uname
+
+class github:
+    def __init__(self, username, pw, pname):
+        self.driver = webdriver.Chrome()
+        self.username = username
+        self.driver.get("https://github.com/login")
+        sleep(2)
+        self.driver.find_element_by_id("login_field")\
+            .send_keys(username)
+        sleep(2)
+        self.driver.find_element_by_id("password")\
+            .send_keys(pw)
+        self.driver.find_element_by_xpath("/html/body/div[3]/main/div/form/div[4]/input[9]")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/aside[1]/div[2]/div[2]/div/h2/a")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input")\
+            .send_keys(pname)
+        sleep(2)
+        self.driver.find_element_by_xpath("/html/body/div[4]/main/div/form/div[3]/button")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_class_name("Header-link")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_class_name("dropdown-item dropdown-signout")\
+            .click()
+        sleep(10)
+
+       
+        
+
+github(uname,pw,'test automation2')
